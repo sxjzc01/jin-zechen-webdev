548aa17d8146d72b5c08bedf69f5c953e142c868
sxjzc01 <jin.ze@husky.neu.edu>
2017-06-08 11:23:48 -0400

    Create app.js

--- public/project/imdbapi/app.js
+++ public/project/imdbapi/app.js
@@ -3,28 +3,23 @@
         .module('pocApp', [])
         .controller('pocController', pocController)
     
-    function pocController($http, $sce) {
+    function pocController($http) {
         var model = this;
         model.searchMovie = searchMovie;
         model.searchDetails = searchDetails;
         
-        model.getImbdUrl = (function (link) {
-            var embedurl = 'http://www.omdbapi.com/?apikey=852159f0&i/'
-            var urlList = link.split('/')
-            return $sce.trustAsResourceUrl(embedurl + urlList[urlList.length - 1])
-        })
 
         function searchDetails(imdbID) {
-            var url = "http://www.omdbapi.com/?apikey=852159f0&i=" + imdbID;
-            $http.jsonp(url)
+            var url = "http://www.omdbapi.com/?apikey=e75522b8&i=" + imdbID;
+            $http.get(url)
                 .then(function (response) {
                     model.movie = response.data;
                 });
         }
         
         function searchMovie(title) {
-            var url = "http://www.omdbapi.com/?apikey=852159f0&s=" + title;
-            $http.jsonp(url)
+            var url = "http://www.omdbapi.com/?apikey=e75522b8&s=" + title;
+            $http.get(url)
                 .then(function (response) {
                     console.log(response);
                     model.movies = response.data.Search;
