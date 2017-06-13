884d2070806b474f5f676c8d098be6d11146f5c5
sxjzc01 <jin.ze@husky.neu.edu>
2017-06-12 23:45:21 -0400

    assignment 5
    assignment 5

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
 var app = require('./express');
+
 // var express = require('express');
+
 // var app = express();
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+
 // configure a public directory to host static content
+
 app.use(app.express.static(__dirname + '/public'));
 
-require ("./test/app.js")(app);
+
+
+// require ("./test/app.js")(app);
+
 require ("./assignment/app/app");
 
+
+
 var port = process.env.PORT || 3000;
+
+
 
 app.listen(port);
