558eceae1609b23cfcb4ce52586283703ff1765e
sxjzc01 <jin.ze@husky.neu.edu>
2017-06-13 12:51:48 -0400

    Update app.js

--- assignment/app/app.js
+++ assignment/app/app.js
@@ -1,7 +1,14 @@
 var app = require('../../express');
 var mongoose = require('mongoose');
 mongoose.Promise = require('q').Promise;
-mongoose.connect('mongodb://localhost/webdev_summer1_2017');
+// mongoose.connect('mongodb://localhost/webdev_summer1_2017');
+
+var connectionString = 'mongodb://127.0.0.1:27017/webdev_summer1_2017'; // for local
+if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+    var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+    var password = process.env.MLAB_PASSWORD_WEBDEV;
+    connectionString = 'mongodb://' + username + ':' + password;
+}
 
 require("../model/models.server.js");
 // require('../model/user/user.schema.server.js');
