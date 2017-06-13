9b89b2004592278be8c0ac864c75a7f245d44415
sxjzc01 <jin.ze@husky.neu.edu>
2017-06-13 13:39:38 -0400

    assignment 5
    assignment 5

--- assignment/app/app.js
+++ assignment/app/app.js
@@ -1,7 +1,17 @@
 var app = require('../../express');
+
+var connectionString = 'mongodb://127.0.0.1:27017/webdev_summer1_2017'; // for local
+if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+    var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+    var password = process.env.MLAB_PASSWORD_WEBDEV;
+    connectionString = 'mongodb://' + username + ':' + password;
+    connectionString += '@ds159517.mlab.com:59517/heroku_r73v1gq7';
+}
 var mongoose = require('mongoose');
 mongoose.Promise = require('q').Promise;
-mongoose.connect('mongodb://localhost/webdev_summer1_2017');
+mongoose.connect(connectionString);
+
+
 
 require("../model/models.server.js");
 // require('../model/user/user.schema.server.js');
